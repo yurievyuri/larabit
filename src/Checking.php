2312c1cb9cce6c7b3c53cf419e62b0b61567cb89
yuriyuriev <yurievyuri@live.com>
2023-04-16 13:54:59 +0100

    test Checking.php

---
+++ src/Checking.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Larabit;
+
+/**
+ * @created on 16/04/2023 by yuriyuriev
+ * dev.server
+ */
+class Checking
+{
+    public static function x(): bool
+    {
+        return true;
+    }
+}
